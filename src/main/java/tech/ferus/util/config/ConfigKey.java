fb9da36419bb61ebe119c4936b72e1f9b7730ca8
Nicholas Badger <awildferus@gmail.com>
2017-04-20 07:47:17 -0400

    Forgot Javadoc on ConfigKey

--- src/main/java/tech/ferus/util/config/ConfigKey.java
+++ src/main/java/tech/ferus/util/config/ConfigKey.java
@@ -26,30 +26,70 @@ import javax.annotation.Nullable;
 
 import static tech.ferus.util.config.ConfigFile.getDefaultConfig;
 
+/**
+ * An object designed to allow easy data-grabbing from a {@link ConfigFile}.
+ *
+ * @param <T> the type of object that this {@link ConfigKey} should reference
+ */
 public class ConfigKey<T> {
 
-    @Nonnull private final String[] key;
+    /**
+     * The path from root to node.
+     */
+    @Nonnull private final Object[] key;
 
-    private ConfigKey(@Nonnull final String key) {
-        this.key = new String[] {key};
-    }
-
-    private ConfigKey(@Nonnull final String[] key) {
+    /**
+     * Constructs a new {@link ConfigKey}.
+     *
+     * @param key the path to the node on any level
+     */
+    private ConfigKey(@Nonnull final Object[] key) {
         this.key = key;
     }
 
+    /**
+     * Returns the value stored in configuration for this key.
+     *
+     * <p>Note: The method attempts to get the data from the default {@link ConfigFile}.</p>
+     *
+     * @return the value stored in configuration for this key
+     */
     @Nullable public T get() {
         return this.get(getDefaultConfig(), null);
     }
 
+    /**
+     * Returns the value stored in configuration for this key.
+     *
+     * @param config the {@link ConfigFile} to look through
+     * @return the value stored in configuration for this key
+     */
     @Nullable public T get(@Nonnull final ConfigFile config) {
         return this.get(config, null);
     }
 
+    /**
+     * Returns the value stored in configuration this key.
+     *
+     * <p>Note: The method attempts to get the data from the default {@link ConfigFile}.</p>
+     *
+     * <p>Additionally, this method will return a default value if none can be found for this key.</p>
+     *
+     * @param def the default value to be returned if an existing value cannot be obtained
+     * @return the value stored in configuration for this key, or default
+     */
     @Nullable public T get(@Nullable final T def) {
         return this.get(getDefaultConfig(), def);
     }
 
+    /**
+     * Returns the value stored in configuration this key.
+     *
+     * <p>Additionally, this method will return a default value if none can be found for this key.</p>
+     *
+     * @param def the default value to be returned if an existing value cannot be obtained
+     * @return the value stored in configuration for this key, or default
+     */
     @SuppressWarnings("unchecked")
     @Nullable
     public T get(@Nullable final ConfigFile config, @Nullable final T def) {
@@ -58,16 +98,31 @@ public class ConfigKey<T> {
         }
 
         try {
-            return (T) config.getNode(this.key).getValue(def);
+            return (T) config.getNode((Object[]) this.key).getValue(def);
         } catch (final ClassCastException e) {
             throw new IllegalArgumentException("Improper value type for \"{}\"!", e);
         }
     }
 
+    /**
+     * Sets and saves a value for the node located at the path for this key.
+     *
+     * <p>Note: This method will attempt to save data to the default {@link ConfigFile}.</p>
+     *
+     * @param value the value to change the node to
+     * @throws IOException if the configuration file cannot be saved to
+     */
     public void set(@Nullable final T value) throws IOException {
         this.set(getDefaultConfig(), value);
     }
 
+    /**
+     * Sets and saves a value for the node located at the path for this key.
+     *
+     * @param config the {@link ConfigFile} to save to
+     * @param value the value to change the node to
+     * @throws IOException if the configuration file cannot be saved to
+     */
     public void set(@Nullable final ConfigFile config, @Nullable final T value) throws IOException {
         if (config == null) {
             throw new IllegalStateException("Attempted to set \"" + String.join(".", this.key) + "\" to a null ConfigFile.");
@@ -77,11 +132,31 @@ public class ConfigKey<T> {
         config.save();
     }
 
-    public static <T> ConfigKey<T> of(final String key) {
-        return new ConfigKey<>(key);
+    /**
+     * Convenience method to construct a new {@link ConfigKey}.
+     *
+     * <p>The purpose of this method is purely to reduce
+     * <code>new ConfigKey(new Object[]{"string"});</code> to <code>ConfigKey.of("string");</code>.</p>
+     *
+     * @param key the path to the node on the top level
+     * @param <T> the type of object that the {@link ConfigKey} should reference
+     * @return a new {@link ConfigKey} with the specified path
+     */
+    public static <T> ConfigKey<T> of(final Object key) {
+        return new ConfigKey<>(new Object[]{key});
     }
 
-    public static <T> ConfigKey<T> of(final String... key) {
+    /**
+     * Convenience method to construct a new {@link ConfigKey}.
+     *
+     * <p>The purpose of this method is purely to reduce
+     * <code>new ConfigKey(new Object[]{"string"});</code> to <code>ConfigKey.of("string");</code>.</p>
+     *
+     * @param key the path to the node on any level
+     * @param <T> the type of object that the {@link ConfigKey} should reference
+     * @return a new {@link ConfigKey} with the specified path
+     */
+    public static <T> ConfigKey<T> of(final Object... key) {
         return new ConfigKey<>(key);
     }
 }
